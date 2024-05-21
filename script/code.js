1529e5e4b9ef7f378138cee7e74ee96bee02c5ec
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-05-21 16:30:02 +0200

    die

--- script/code.js
+++ script/code.js
@@ -115,7 +115,7 @@ switch (true) {
         break
 } */
 
-let grade = ""
+/* let grade = ""
 let mark = 50
 switch (true) {
     case mark == 100:
@@ -147,4 +147,82 @@ switch (true) {
         break
 }
 
-console.log(`${grade}`)
+console.log(`${grade}`) */
+
+
+
+
+
+
+
+/* function greeting(firstName , lastName){
+    console.log(`My name is ${firstName} ${lastName}`)
+}
+
+//argument
+greeting('Joel','Mukanya')
+greeting('Robyn','Carnow') */
+
+//Addition function to display the sum of 2 numbers
+
+/* function add(num1, num2 /* = 2   a default value that you can assign){
+    let add = num1 + num2 // if a value isnt defined you will get an output of NaN
+    console.log(add)
+}*/
+
+//function add(...storage) { // ...storage is an array called storage
+/* console.log(storage.reduce((curr, n) => curr + n)) */
+/*     console.log(storage.reduce(addition))
+
+    if (Number.isInteger(addition)) {
+        console.log(addition)
+    } 
+    else{
+        console.log(0)
+    } //doesnt work properly
+
+} */
+/* 
+add(4, 86)
+add(4, 86, 76)
+add(4, 86, 23, 54) // these get pushed into the variables */
+
+/* add('Sipho', 1, 'Jamin', 2, 3, 'Leah', 4, 2, 5, 6, 'Joel')
+
+function addition(curr, nextV) {
+    return curr + nextV
+} */
+
+
+//let arr = ['John','Arnold','Richie','Alex','Kino'] 
+
+
+function ex(...arr) {
+    arr.forEach((element) => {
+     //   console.log(element)
+        if (element == 2){
+            console.log('ostrich')
+        } else {
+            console.log('Oct')
+        } }     
+    );
+   // console.log(arr)
+}
+
+ex(2 , 'John', 9, 6, 'Arnold', 4 , 2 , 'Richie', 'Alex', 'Kino')
+
+
+function ex2(...arr1){
+for (let i = 0; i < arr1.length; i++ ){
+    console.log(arr1[i])
+}
+}
+
+ex2('bob' , 34 , 'Andre' , 5) 
+
+let data = [9,6,'64','pop','op',4]
+
+data.forEach((value) => {
+
+    console.log(value.startsWith(value))
+})
